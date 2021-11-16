4ed2c3d922d7dca69afd2c4699032978d52a5c22
FranciscoMillan <f3millan@gmail.com>
2021-11-16 09:57:41 -0500

    commit tree 04d12802e18a1bc1a866dbf8d1333764018724d0

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 
 // Write your code here
+
+/*function multiply (num1,num2){
+    return (num1*num2); 
+}
+multiply (62,1)
+*/
+
+let num1 = 62;
+let num2= 1;
+
+const multiply = num1 * num2;
+
+multiply;
+
+/*function random (vaule){
+    Number.isInteger(vaule);
+    return Number.isInteger(vaule);
+}
+random (42);*/
+
+
+//random
+let random = 42;
+
+ Number.isInteger(random);
+
+ //remainder
+
+ let num3 = 19 ;
+ let num4 = 5 ;
+
+ const mod = num3 % num4;
+
+ mod;
+
+//¡MAX!
+
+const max = Math.max(5,10,15,20);
+max;
+
+
+
